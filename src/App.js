7dde76b8fff190d8868f048ea20116dd92d76be7
Jason Barrantes <jasonbar1998@gmail.com>
2021-11-26 16:24:00 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React, { useRef } from "react";
+import "./App.css";
+import * as tf from "@tensorflow/tfjs";
+import * as posenet from "@tensorflow-models/posenet";  // import posenet from tensorflow models
+import Webcam from "react-webcam";                      // webcam function from react
+import { drawKeypoints, drawSkeleton } from "./utilities";  // to draw
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  let lista = [];
+  //  Load posenet
+  const runPosenet = async () => {
+    const net = await posenet.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8,
+    });
+    
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Make Detections
+      const pose = await net.estimateSinglePose(video);
+      //console.log(pose);
+      
+      for (let i = 0; i < pose.keypoints.length; i++) {
+        console.log(pose.keypoints[i])
+        if(pose.keypoints[i].score > 0.2){
+          lista.push(pose.keypoints[i].part)
+        } 
+      }
+      
+
+      
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext("2d");
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose["keypoints"], 0.6, ctx);
+    drawSkeleton(pose["keypoints"], 0.7, ctx);
+  };
+
+  runPosenet();
+
+  return (
+    <div className="App">
+      <div style={{ height: 50, marginTop:10, textAlign: 'center'}}> IA - Examen 2 Parte 2   Jason Barrantes</div>
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+      <div >
+        wait for the points
+      </div>
+    </div>
+  );
+}
+
+export default App;
